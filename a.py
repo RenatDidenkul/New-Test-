76a2bdb6ac6f1af2bfd753e90f5e2417230152b0
Nezer4950 <104249771+Nezer4950@users.noreply.github.com>
2024-03-17 11:29:04 +0200

    Delete a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-def average(lst):
-    if not lst:
-        return 0
-    return sum(lst) / len(lst)
-
-numbers = list(map(int, input().split()))
-print("Середнє значення списку:", average(numbers))
-    
